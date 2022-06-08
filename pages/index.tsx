ce0b7858209e11292c9ba17d375162e69c786180
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2022-06-08 02:20:17 +0100

    Add Navbar component and styles for desktop

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,23 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Scholarly Africa</title>
         <meta name="description" content="Scholarly official website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://scholarly.africa/">Scholarly!</a>
-        </h1>
+      <main>
+        <Navbar />
       </main>
 
-      <footer className={styles.footer}>
-      </footer>
+      {/* <footer className={styles.footer}></footer> */}
     </div>
   )
 }
