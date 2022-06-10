e21d5fee3b53b72163accaf16ecb07d628d4d9c6
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2022-06-10 01:50:21 +0100

    Update colors theme

--- theme/colors.ts
+++ theme/colors.ts
@@ -1,16 +1,17 @@
 const colors = {
   brand: {
     yellow: '#F7B71D',
-    darkGreen: '#233D2C',
+    nearWhite: '#F5F6F6',
+    green: '#415749',
     lime: {
-      50: '#e5fbeb',
+      50: '#E9F5ED',
       100: '#c4ead0',
       200: '#a2dbb4',
       300: '#7fcb97',
       400: '#5cbd7a',
       500: '#46AC66',
       600: '#327f4a',
-      700: '#225b35',
+      700: '#233D2C',
       800: '#11371e',
       900: '#001405',
     }
