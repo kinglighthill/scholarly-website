3e6e60c28f47816ba5e0cdaceef6b10ef467611e
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2022-06-18 01:11:24 +0100

    Mock data that should come from the API

---
+++ data.ts
@@ -0,0 +1,15 @@
+import { TestimonialProps } from "./components/reusables/Testimonial";
+
+export const statistics = [
+  { title: 'Apps', value: '30+' },
+  { title: 'Downloads', value: '1 million+' },
+  { title: 'Partners', value: '20,000+' },
+  { title: 'Businesses', value: '600+' },
+]
+
+export const testimonials: TestimonialProps[] = [
+  { name: 'Umeh Chibuzor', title: 'Student', image: 'jrotanson.svg', rating: 4, message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non pellentesque faucibus sed in nunc Lorem ipsum dolor sit  consectetur adipiscing elit. Lorem ipsum dolor sit amet.' },
+  { name: 'Umeh Chibuzor', title: 'Agent', image: '', rating: 5, message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non pellentesque faucibus sed in nunc Lorem ipsum dolor sit  consectetur adipiscing elit. Lorem ipsum dolor sit amet.' },
+  { name: 'Umeh Dhibuzor', title: 'Agent', image: '', rating: 5, message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non pellentesque faucibus sed in nunc Lorem ipsum dolor sit  consectetur adipiscing elit. Lorem ipsum dolor sit amet.' },
+  { name: 'Umeh Xhibuzor', title: 'Agent', image: '', rating: 5, message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non pellentesque faucibus sed in nunc Lorem ipsum dolor sit  consectetur adipiscing elit. Lorem ipsum dolor sit amet.' },
+]
