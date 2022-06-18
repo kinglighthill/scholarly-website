824960545bd118bf3e80d18d50aa0e1a661cca74
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2022-06-18 06:35:51 +0100

    Update styles

--- components/partners/Step.tsx
+++ components/partners/Step.tsx
@@ -11,14 +11,14 @@ type StepProps = {
 
 export default function Step({ index, title, description, icon, direction }: StepProps) {
   return (
-    <HStack justify='space-between' w='50%' flexDir={direction === 'forward' ? 'row' : 'row-reverse'}>
+    <HStack justify={direction === 'forward' ? 'space-between' : 'start'} w='50%' flexDir={direction === 'forward' ? 'row' : 'row-reverse'}>
       <VStack spacing={4} align='start' ml={direction === 'reverse' ? '21%' : 0} mr={direction === 'forward' ? '20%' : 0}>
         <Text fontSize={25} fontWeight='bold'>
           <Text as='span' color='brand.lime.500'>Step {index}: </Text><Text as='span' color='brand.lime.700'>{title}</Text>
         </Text>
         <Text color='brand.lime.700'>{description}</Text>
       </VStack>
-      <Box>
+      <Box minW='15%'>
         <Image src={icon} alt='' />
       </Box>
     </HStack>
