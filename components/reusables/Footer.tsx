fbcaeea9feb44ea433ce3513739caa3b52ebd2f3
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2022-06-23 04:53:53 +0100

    Update footer

--- components/reusables/Footer.tsx
+++ components/reusables/Footer.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, HStack, Icon, Spacer, Text, VStack } from '@chakra-ui/react';
+import { Box, Flex, HStack, Icon, SimpleGrid, Spacer, Text, VStack } from '@chakra-ui/react';
 import Image from 'next/image';
 import Link from 'next/link';
 import scholarly_logo2 from '../../public/scholarly_logo2.svg';
@@ -10,90 +10,100 @@ import whatsapp from '../../public/whatsapp.svg';
 import classes from '../../styles/components/Footer.module.css';
 import FooterMenu from '../footer/FooterMenu';
 import { PhoneIcon, AtSymbolIcon } from '@heroicons/react/outline';
+import FooterAccordion from '../footer/FooterAccordion';
 
 export default function Footer() {
   return (
     <Box as='footer' bgColor='brand.lime.700' className={classes.footer_container}>
-      <Link href='/' passHref>
-        <a className={classes.logo_wrapper}>
-          <HStack spacing={1} align='end'>
-            <Image src={scholarly_logo2} alt='Scholarly Logo' />
-            <Text fontWeight='bold' color='white' fontSize={24}>Scholarly</Text>
+      <Box display={{base: 'none', lg: 'block'}}>
+        <Link href='/' passHref>
+          <a className={classes.logo_wrapper}>
+            <HStack spacing={1} align='end'>
+              <Image src={scholarly_logo2} alt='Scholarly Logo' />
+              <Text fontWeight='bold' color='white' fontSize={24}>Scholarly</Text>
+            </HStack>
+          </a>
+        </Link>
+
+        <Flex mb='50px'>
+          <FooterMenu title='Products'
+            items={[
+              { name: 'Mobile Apps', url: '/' },
+              { name: 'Desktop Apps', url: '/' },
+              { name: 'Partner Apps', url: '/' },
+              { name: 'Enterprise Software', url: '/' }
+            ]}
+          />
+          <Spacer />
+          <FooterMenu title='Company'
+            items={[
+              { name: 'About', url: '/' },
+              { name: 'FAQs', url: '/' },
+              { name: 'Contact us', url: '/' },
+            ]}
+          />
+          <Spacer />
+          <FooterMenu title='Legal'
+            items={[
+              { name: 'Terms', url: '/' },
+              { name: 'Privacy', url: '/' },
+              { name: 'Security', url: '/' },
+            ]}
+          />
+          <Spacer />
+          <Box>
+            <Box p='25px 14px' mb={4} borderRadius='30px 0' background='rgba(255,255,255,0.2)' maxW='273px'>
+              <VStack spacing='26px'>
+                <Box>
+                  <HStack spacing={1.5} color='white'>
+                    <Icon as={PhoneIcon} />
+                    <Link href='tel:+23490-6170-1939'>+23490-6170-1939</Link>
+                  </HStack>
+                  <HStack spacing={1.5} color='brand.yellow'>
+                    <Icon as={AtSymbolIcon} />
+                    <Link href='mailto:scholarlyafrica@gmail.com'>scholarlyafrica@gmail.com</Link>
+                  </HStack>
+                </Box>
+                <Text color='white' fontSize={13} textAlign='right'>Samuelson close, near FGC. Independence Layout, Enugu, Nigeria</Text>
+              </VStack>
+            </Box>
+            <HStack spacing={4}>
+              <a href='https://facebook.com' aria-label='Visit our facebook page'>
+                <Image src={facebook} alt='Facebook Icon' />
+              </a>
+              <a href='https://twitter.com' aria-label='Visit our twitter handle'>
+                <Image src={twitter} alt='Twitter Icon' />
+              </a>
+              <a href='https://linkedin.com' aria-label='Visit our linkedin page'>
+                <Image src={linkedin} alt='LinkedIn Icon' />
+              </a>
+              <a href='https://instagram.com' aria-label='Visit our instagram page'>
+                <Image src={instagram} alt='Instagram Icon' />
+              </a>
+              <a href='https://wa.me/+2349061701939' aria-label='Send us a message on Whatsapp'>
+                <Image src={whatsapp} alt='WhatsApp Icon' />
+              </a>
+            </HStack>
+          </Box>     
+        </Flex>
+
+        <Text maxW='656px' m='auto' color='white' textAlign='center' fontWeight='light'>
+          Scholarly is a leading educational technology company focused in Africa. With over 3 years experience, we provide better learning tools and resources to students for better grade.
+        </Text>
+
+        <Flex color='white' fontWeight='light' mt={8}>
+          <Text>Copyright 2020 Scholarly educational software, Inc. All Rights reserved</Text>
+          <Spacer />
+          <HStack spacing={8}>
+            <Link href='/'>Privacy</Link>
+            <Link href='/'>Terms of use</Link>
           </HStack>
-        </a>
-      </Link>
-
-      <HStack align='start' justify='end' spacing='100px' mb='50px'>
-        <FooterMenu title='Products'
-          items={[
-            { name: 'Mobile Apps', url: '/' },
-            { name: 'Desktop Apps', url: '/' },
-            { name: 'Partner Apps', url: '/' },
-            { name: 'Enterprise Software', url: '/' }
-          ]}
-        />
-        <FooterMenu title='Company'
-          items={[
-            { name: 'About', url: '/' },
-            { name: 'FAQs', url: '/' },
-            { name: 'Contact us', url: '/' },
-          ]}
-        />
-        <FooterMenu title='Legal'
-          items={[
-            { name: 'Terms', url: '/' },
-            { name: 'Privacy', url: '/' },
-            { name: 'Security', url: '/' },
-          ]}
-        />
-        <Box>
-          <Box p='25px 14px' mb={4} borderRadius='30px 0' background='rgba(255,255,255,0.2)' maxW='273px'>
-            <VStack spacing='26px'>
-              <Box>
-                <HStack spacing={1.5} color='white'>
-                  <Icon as={PhoneIcon} />
-                  <Link href='tel:+23490-6170-1939'>+23490-6170-1939</Link>
-                </HStack>
-                <HStack spacing={1.5} color='brand.yellow'>
-                  <Icon as={AtSymbolIcon} />
-                  <Link href='mailto:scholarlyafrica@gmail.com'>scholarlyafrica@gmail.com</Link>
-                </HStack>
-              </Box>
-              <Text color='white' fontSize={13} textAlign='right'>Samuelson close, near FGC. Independence Layout, Enugu, Nigeria</Text>
-            </VStack>
-          </Box>
-          <HStack spacing={4}>
-            <a href='https://facebook.com' aria-label='Visit our facebook page'>
-              <Image src={facebook} alt='Facebook Icon' />
-            </a>
-            <a href='https://twitter.com' aria-label='Visit our twitter handle'>
-              <Image src={twitter} alt='Twitter Icon' />
-            </a>
-            <a href='https://linkedin.com' aria-label='Visit our linkedin page'>
-              <Image src={linkedin} alt='LinkedIn Icon' />
-            </a>
-            <a href='https://instagram.com' aria-label='Visit our instagram page'>
-              <Image src={instagram} alt='Instagram Icon' />
-            </a>
-            <a href='https://wa.me/+2349061701939' aria-label='Send us a message on Whatsapp'>
-              <Image src={whatsapp} alt='WhatsApp Icon' />
-            </a>
-          </HStack>
-        </Box>     
-      </HStack>
-
-      <Text maxW='656px' m='auto' color='white' textAlign='center' fontWeight='light'>
-        Scholarly is a leading educational technology company focused in Africa. With over 3 years experience, we provide better learning tools and resources to students for better grade.
-      </Text>
-
-      <Flex color='white' fontWeight='light' mt={8}>
-        <Text>Copyright 2020 Scholarly educational software, Inc. All Rights reserved</Text>
-        <Spacer />
-        <HStack spacing={8}>
-          <Link href='/'>Privacy</Link>
-          <Link href='/'>Terms of use</Link>
-        </HStack>
-      </Flex>
+        </Flex>
+      </Box>
+      
+      <Box display={{lg: 'none'}}>
+        <FooterAccordion />
+      </Box>
     </Box>
   )
 }
