84e6d0aff4b49224c5bc0ce6ee44e9fd807a0902
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2022-07-14 21:55:05 +0100

    Override the default component styles for Chakra UI's Checkbox component

---
+++ theme/components/checkbox.ts
@@ -0,0 +1,16 @@
+const checkbox = {
+  baseStyle: {
+    control: {
+      border: '1.5px solid',
+      borderColor: 'brand.lime.700',
+      _disabled: {
+        borderColor: 'brand.lime.700',        
+      }
+    },
+    label: {
+      m: 0
+    }
+  },
+}
+
+export default checkbox;
