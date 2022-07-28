14845ec52a1cc6d61c20e78855820726280f9a5b
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2022-07-28 08:55:09 +0100

    Update responsive styles for Get Started page

--- components/get_started/Card.tsx
+++ components/get_started/Card.tsx
@@ -5,11 +5,11 @@ import { GetStartedCardProps } from "../../types/components/get_started/card";
 
 export default function GetStartedCard({ title, description, btnText, btnHref }: GetStartedCardProps) {
   return (
-    <Box pt={7} pb={5} px={5} bg="rgba(255, 255, 255, 0.2)" borderRadius={8}>
-      <Text as="h2" fontSize={25} mb={6} color="white">{title}</Text>
+    <Box pt={7} pb={5} px={5} bg="rgba(255, 255, 255, 0.2)" borderRadius={8} display="flex" flexDir="column" justifyContent="space-between">
+      <Text as="h2" mb={6} color="white" fontSize={25} fontWeight="medium">{title}</Text>
       <Text mb={4} color="white">{description}</Text>
       <Link href={btnHref}>
-        <Button type="button" variant="solid" bg="white" color="brand.lime.500" _hover={{bg: "gray.100"}}>
+        <Button type="button" variant="solid" w="max-content" bg="white" color="brand.lime.500" _hover={{bg: "gray.200"}}>
           Continue
           <Icon as={ArrowNarrowRightIcon} ml={4} />
         </Button>
