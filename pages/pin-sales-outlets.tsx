c0328a5513104c656496c0d39062a4b0da314657
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2022-08-15 18:59:17 +0100

    Create "coming soon" pages for pin sales outlets, and CBT and learning centres

---
+++ pages/pin-sales-outlets.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+import Page from "../components/reusables/Page";
+import ComingSoon from "../components/reusables/ComingSoon";
+
+const PinSalesOutlets: NextPage = () => {
+  return (
+    <Page title="Scholarly Africa | Pin Sales Outlets">
+      <ComingSoon />
+    </Page>
+  )
+}
+
+export default PinSalesOutlets;
