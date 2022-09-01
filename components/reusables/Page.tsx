cc9a0c139ec2aac21ef147ecd8edd612185ea7e3
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2022-09-01 19:09:38 +0100

    Add a description meta tag and update the type definitions accordingly

--- components/reusables/Page.tsx
+++ components/reusables/Page.tsx
@@ -3,11 +3,12 @@ import { PageProps } from "../../types/components/reusables/page";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
-export default function Page({ children, title }: PageProps) {
+export default function Page({ children, title, description }: PageProps) {
   return (
     <div>
       <Head>
         <title>{title}</title>
+        <meta name="description" content={description} />
       </Head>
       <Navbar />
       
