f02202d7826c2442e9df565a90614a7dd9b97c60
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2022-09-05 11:21:34 +0100

    Update the "coming soon" component
    - Change email field to a contolled component
    - Add extra validation for email input
    - Show error message for invalid email inputs
    - Consume API for submitting the user's email
    - Show loading status when form is submitted

--- components/reusables/ComingSoon.tsx
+++ components/reusables/ComingSoon.tsx
@@ -1,11 +1,48 @@
-import { Box, Button, Flex, FormControl, FormLabel, Input, Spacer, Text, VStack } from "@chakra-ui/react";
+import { useState } from "react";
 import Image from "next/image";
+import { Box, Button, Flex, FormControl, FormErrorMessage, FormLabel, Input, Spacer, Text, VStack } from "@chakra-ui/react";
 import coming_soon from "../../public/coming_soon.svg";
+import { notifyUser } from "../../services/notification.service";
+import useCustomToast from "../../hooks/useCustomToast";
 
-export default function ComingSoon() {
-  const handleSubmit = (e: React.FormEvent) => {
+export default function ComingSoon({ feature }: {feature: string}) {
+  const makeToast = useCustomToast();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>('');
+  const [emailError, setEmailError] = useState<boolean>(false);
+  // Regex pattern coined from https://www.regular-expressions.info/email.html
+  const emailPattern: RegExp = /^[A-Z0-9][A-Z0-9._%+-]+?@(?:[A-Z0-9-]+\.)+[A-Z]{2,}$/i;
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    if (emailPattern.test(value) && emailError) {
+      setEmailError(false);
+    }
+    setEmail(value);
+  }
+
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    console.log((e.target as HTMLFormElement).email.value);
+    if (!emailPattern.test(email)) {
+      setEmailError(true);
+      return;
+    };
+
+    try {
+      setLoading(true);
+      const payload = { email, feature }
+      const response = await notifyUser(payload);
+      if (response.ok) {
+        makeToast('Email submitted successfully', "We'll notify you when this feature is available.");
+        setEmail('');
+        setLoading(false);
+      }
+    }
+    catch (error) {
+      console.log(error);
+      makeToast('An error occured', "Please ensure that you're connected to the internet and try again.", 'error');
+      setLoading(false);
+    }
   }
 
   return (
@@ -20,11 +57,14 @@ export default function ComingSoon() {
           <Text color="brand.lime.700" fontSize={13}>Our team is working hard to develop this product. Kindly provide your email so we&apos;ll inform you when we launch.</Text>
           <Text color="brand.lime.500" fontWeight="medium">Be the first to know when we launch</Text>
           <form aria-label="Join the waitlist" onSubmit={handleSubmit} style={{width: "100%"}}>
-            <FormControl mb={6}>
+            <FormControl mb={6} isRequired isInvalid={emailError}>
               <FormLabel htmlFor="email" mb={2} fontSize={13} fontWeight="medium">Email</FormLabel>
-              <Input type="email" id="email" name="email" placeholder="Enter your email" fontSize={13} focusBorderColor="brand.lime.500" />
+              <Input id="email" type="email" name="email" value={email} placeholder="Enter your email" fontSize={13}
+                focusBorderColor="brand.lime.500" onChange={handleChange}
+              />
+              <FormErrorMessage>Email is invalid.</FormErrorMessage>
             </FormControl>
-            <Button type="submit" variant="solid">Join waitlist</Button>
+            <Button type="submit" variant="solid" isLoading={loading}>Join waitlist</Button>
           </form>
         </VStack>
       </Flex>
