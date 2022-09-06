11b89d1b5857615fb254a4e893297f9d0d1ebd9b
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2022-09-06 08:35:22 +0100

    Abstract shared logic into a custom hook

--- components/reusables/ComingSoon.tsx
+++ components/reusables/ComingSoon.tsx
@@ -1,49 +1,10 @@
-import { useState } from "react";
 import Image from "next/image";
 import { Box, Button, Flex, FormControl, FormErrorMessage, FormLabel, Input, Spacer, Text, VStack } from "@chakra-ui/react";
 import coming_soon from "../../public/coming_soon.svg";
-import { notifyUser } from "../../services/notification.service";
-import useCustomToast from "../../hooks/useCustomToast";
+import useFeatureNotifier from "../../hooks/useFeatureNotifier";
 
 export default function ComingSoon({ feature }: {feature: string}) {
-  const makeToast = useCustomToast();
-  const [loading, setLoading] = useState<boolean>(false);
-  const [email, setEmail] = useState<string>('');
-  const [emailError, setEmailError] = useState<boolean>(false);
-  // Regex pattern coined from https://www.regular-expressions.info/email.html
-  const emailPattern: RegExp = /^[A-Z0-9][A-Z0-9._%+-]+?@(?:[A-Z0-9-]+\.)+[A-Z]{2,}$/i;
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    if (emailPattern.test(value) && emailError) {
-      setEmailError(false);
-    }
-    setEmail(value);
-  }
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    if (!emailPattern.test(email)) {
-      setEmailError(true);
-      return;
-    };
-
-    try {
-      setLoading(true);
-      const payload = { email, feature }
-      const response = await notifyUser(payload);
-      if (response.ok) {
-        makeToast('Email submitted successfully', "We'll notify you when this feature is available.");
-        setEmail('');
-        setLoading(false);
-      }
-    }
-    catch (error) {
-      console.log(error);
-      makeToast('An error occured', "Please ensure that you're connected to the internet and try again.", 'error');
-      setLoading(false);
-    }
-  }
+  const { email, emailError, loading, handleChange, handleSubmit } = useFeatureNotifier(feature);
 
   return (
     <Box as="section" py={24} px={[5, 12, 7, 14, 24]}>
