a782dc570220bc46be3cacf732baebda29b1781e
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2022-09-10 17:52:28 +0100

    Update API key

--- services/userlocation.service.ts
+++ services/userlocation.service.ts
@@ -2,5 +2,5 @@ import { getRequest } from "./default.service";
 
 export const getUserLocation = (lat: number, long: number) => {
   const encodedURL = encodeURIComponent(`${lat},${long}`);
-  return getRequest(`https://api.opencagedata.com/geocode/v1/json?q=${encodedURL}&key=81235eaee3b340a2a988ff1c474afde3`);
+  return getRequest(`https://api.opencagedata.com/geocode/v1/json?q=${encodedURL}&key=2906e0a8699c480baa0ea9c9d94e4015`);
 }
