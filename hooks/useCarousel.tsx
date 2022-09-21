3c6ed7257bce3519892b66f427d55f03674f93bb
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2022-09-21 18:04:14 +0100

    - Update the implementation of the hook

--- hooks/useCarousel.tsx
+++ hooks/useCarousel.tsx
@@ -1,5 +1,7 @@
-import { RefObject, useState } from "react";
+import { RefObject, useEffect, useState } from "react";
 import AliceCarousel from "react-alice-carousel";
+
+// This is an experimental feature. Use with caution!!!
 
 // Type declaration for the 'responsive' prop of the Alice Carousel component
 export type Breakpoints = {
@@ -9,7 +11,9 @@ export type Breakpoints = {
 }
 
 export default function useCarousel(items: any[], breakpoints: Breakpoints, ref: RefObject<AliceCarousel>) {
-  const [activeIndex, setActiveIndex] = useState<number>(0);
+  // const [activeIndex, setActiveIndex] = useState<number>(0);
+  const element = ref.current;
+  const activeIndex = element?.state?.activeIndex;
 
   // Determine the gap between consecutive slides based on the number of items displayed at once
   const determineIndexGap = (breakpoints: Breakpoints) => {
@@ -23,42 +27,46 @@ export default function useCarousel(items: any[], breakpoints: Breakpoints, ref:
     return breakpoints[exactMatch].items;
   }
 
-  const handleNext = (element: AliceCarousel, gap: number) => {
+  const handleNext = (gap: number) => {
     // If the computation for the next index in the slide is greater than the index of the last slide item (in the array), continue count from the front of the array
     // This helps to achieve infinite scrolling for the slide
-    if (activeIndex + gap > items.length - 1) {
-      const nextIndex = (activeIndex + gap) - items.length;
-      element.slideTo(nextIndex);
-      setActiveIndex(nextIndex);
-      return;
+    if (activeIndex) {
+      if (activeIndex + gap > items.length - 1) {
+        const nextIndex = (activeIndex + gap) - items.length;
+        element.slideTo(nextIndex);
+        // setActiveIndex(nextIndex);
+        return;
+      }
+      element.slideTo(activeIndex + gap);
+      // setActiveIndex(activeIndex + gap);
     }
-    element.slideTo(activeIndex + gap);
-    setActiveIndex(activeIndex + gap);
   }
 
   // If the computation for the next index in the slide is less than the index of the first slide item (in the array), continue count from the end of the array
   // This helps to achieve infinite scrolling for the slide
-  const handlePrevious = (element: AliceCarousel, gap: number) => {
-    if (activeIndex - gap < 0) {
-      const nextIndex = items.length + (activeIndex - gap);
-      element.slideTo(nextIndex);
-      setActiveIndex(nextIndex);
-      return;
+  const handlePrevious = (gap: number) => {
+    if (activeIndex) {
+      if (activeIndex - gap < 0) {
+        const nextIndex = items.length + (activeIndex - gap);
+        element.slideTo(nextIndex);
+        // setActiveIndex(nextIndex);
+        return;
+      }
+      element.slideTo(activeIndex - gap);
+      // setActiveIndex(activeIndex - gap);
     }
-    element.slideTo(activeIndex - gap);
-    setActiveIndex(activeIndex - gap);
   }
 
   const handleNavigation = (trigger: string) => {
     const gap = determineIndexGap(breakpoints);
-    if (ref.current) {
+    // if (ref.current) {
       if (trigger === 'prev') {
-        handlePrevious(ref.current, gap);
+        handlePrevious(gap);
       }
       else if (trigger === 'next') {
-        handleNext(ref.current, gap);
+        handleNext(gap);
       }
-    }
+    // }
   }
   
   return ({
