e90fb1585b72eafd947d284932243a07f840d763
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2022-09-22 18:46:25 +0100

    Correct mistake in the acronym

--- components/home/FAQs.tsx
+++ components/home/FAQs.tsx
@@ -8,7 +8,7 @@ import Questions from '../faqs/Questions';
 export default function FAQs({ data }: { data: FAQ[]}) {
   return (
     <Box as='section' bg='brand.nearWhite' className={classes.faqs_section}>
-      <Text as='h2' color='brand.lime.700' mb={8} textAlign='center' fontSize={[25, 31]} fontWeight='bold'>Frequently Asked Questions (FAQS)</Text>
+      <Text as='h2' color='brand.lime.700' mb={8} textAlign='center' fontSize={[25, 31]} fontWeight='bold'>Frequently Asked Questions (FAQs)</Text>
       <Tabs variant='unstyled' isLazy maxW="min(800px, 95%)" m="auto">
         <TabList mb={9} color="brand.lime.700" justifyContent="center">
           <Tab pl={0} _selected={{pl: 4, borderRadius: 4, color: "white", bg: "brand.lime.500"}}>Students</Tab>
