e2d7c80b32bed4c5ab1fe88d2f12dcca689c4d73
Chukwuamaka Osuji <osujichukwuamaka@gmail.com>
2023-09-13 12:38:12 +0100

    Create a custom hook for handling the display of team member details

---
+++ hooks/useEmployeeDisclosure.tsx
@@ -0,0 +1,21 @@
+import { useDisclosure } from "@chakra-ui/react";
+import { useState } from "react";
+import { EmployeeCardProps } from "../components/team/EmployeeCard";
+import { TeamMember } from "../types/data";
+
+export default function useEmployeeDisclosure() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [employee, setEmployee] = useState<TeamMember>({name: '', position: '', picture: '', bio: ''});
+
+  const showEmployeeDetails: EmployeeCardProps['showEmployeeDetails'] = (employee) => {
+    setEmployee(employee);
+    onOpen();
+  }
+  
+  return ({
+    isOpen,
+    onClose,
+    employee,
+    showEmployeeDetails
+  })
+}
