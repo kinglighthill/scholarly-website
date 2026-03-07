7b68f3bfe155a393fb6847480db7845071726715
Kingsley Ugwudinso <kingholyhill@gmail.com>
2026-03-07 12:04:37 +0100

    changes

--- pages/api/download.ts
+++ pages/api/download.ts
@@ -1,35 +1,35 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from "next";
 import { getStorage, ref, getDownloadURL } from "firebase/storage";
-import app from '../../config/conn';
+import app from "../../config/conn";
 
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
-import { S3, S3Client, GetObjectCommand } from "@aws-sdk/client-s3"
-import AWS from "aws-sdk"
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { S3, S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import AWS from "aws-sdk";
 
 AWS.config.update({
-    accessKeyId: process.env.SPACES_ACCESS_KEY,
-    secretAccessKey: process.env.SPACES_SECRET_KEY,
-    region: "us-east-1",
-})
+  accessKeyId: process.env.SPACES_ACCESS_KEY,
+  secretAccessKey: process.env.SPACES_SECRET_KEY,
+  region: "us-east-1",
+});
 
 const s3 = new AWS.S3({
-    endpoint: "https://scholarly-website-assets.fra1.cdn.digitaloceanspaces.com",
-})
+  endpoint: "https://scholarly-website-assets.fra1.cdn.digitaloceanspaces.com",
+});
 
 async function getDownloadUrl(fileName: string, isPartnersApp: boolean) {
-    const url = isPartnersApp ? fileName : `apps/${fileName}`
+  const url = isPartnersApp ? fileName : `apps/${fileName}`;
 
-    const params = {
-        Bucket: 'scholarly-website-assets',
-        Key: url,
-        Expires: 3600,
-    }
+  const params = {
+    Bucket: "scholarly-website-assets",
+    Key: url,
+    Expires: 3600,
+  };
 
-    const signedUrl = s3.getSignedUrl('getObject', params)
-    return signedUrl.replace(
-        "scholarly-website-assets.scholarly-website-assets.fra1.cdn.digitaloceanspaces.com", 
-        "scholarly-website-assets.fra1.cdn.digitaloceanspaces.com"
-    )
+  const signedUrl = s3.getSignedUrl("getObject", params);
+  return signedUrl.replace(
+    "scholarly-website-assets.scholarly-website-assets.fra1.cdn.digitaloceanspaces.com",
+    "scholarly-website-assets.fra1.cdn.digitaloceanspaces.com",
+  );
 }
 
 // async function getDownloadUrl(fileName: string, isPartnersApp: boolean) {
@@ -41,36 +41,40 @@ async function getDownloadUrl(fileName: string, isPartnersApp: boolean) {
 // }
 
 const windowsFileName = (fileName: string): string => {
-    switch (fileName) {
-        case "jamb-utme":
-            return "Scholarly Jamb CBT"
-        default:
-            return fileName
-    }
-}
+  switch (fileName) {
+    case "jamb-utme":
+      return "Scholarly Jamb CBT";
+    default:
+      return fileName;
+  }
+};
 
 function getAbsoluteFileName(fileName: string, platform: string): string {
-    switch (platform) {
-        case "ios":
-            return `${fileName}.ipa`
-        case "windows":
-            return `windows/${windowsFileName(fileName)}.msi`
-        default:
-            return `${fileName}.apk`
-    }
+  switch (platform) {
+    case "ios":
+      return `${fileName}.ipa`
+    case "windows":
+      return `windows/${windowsFileName(fileName)}.msi`
+    default:
+      return `android/${fileName}.apk`
+  }
 }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const fileName = req.query.fileName as string
-    const platform = req.query.platform as string || "android"
-    const isPartnersApp = (req.query.is_partners_app as string || 'false') === 'true'
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const fileName = req.query.fileName as string;
+  const platform = (req.query.platform as string) || "android";
+  const isPartnersApp =
+    ((req.query.is_partners_app as string) || "false") === "true";
 
-    try {
-        const absoluteFileName = getAbsoluteFileName(fileName, platform)
-        const downloadUrl = await getDownloadUrl(absoluteFileName, isPartnersApp)
-        res.redirect(downloadUrl);
-    } catch (error) {
-        console.error('Error fetching download URL:', error)
-        res.redirect('/404')
-    }
+  try {
+    const absoluteFileName = getAbsoluteFileName(fileName, platform);
+    const downloadUrl = await getDownloadUrl(absoluteFileName, isPartnersApp);
+    res.redirect(downloadUrl);
+  } catch (error) {
+    console.error("Error fetching download URL:", error);
+    res.redirect("/404");
+  }
 }
